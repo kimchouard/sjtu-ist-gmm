09a865844859c76446b4b60ec23cf494fc492b49
Kim CHOUARD <kimchouard@Kims-MacBook-Pro.local>
2014-11-23 11:07:32 +0800

    Can describe set just by counting

--- Set.py
+++ Set.py
@@ -1,9 +1,12 @@
 class SetClass:
     points = []
 
-    def describe(self):
-        for point in self.points:
-            point.describe()
+    def describe(self, full):
+        print "Containing %d points." % len(self.points)
+
+        if full:
+            for point in self.points:
+                point.describe()
 
     def add(self, point):
         self.points.append(point)
